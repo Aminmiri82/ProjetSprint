2178e55f4795b3f3adbcd015014616679c58d3b2
Amin MIRI <miri.amin96@gmail.com>
2023-12-16 18:21:32 +0100

    fixed a bug in the planner, it's now feature complete

--- code/agentPage/plannerCode.js
+++ code/agentPage/plannerCode.js
@@ -1,7 +1,8 @@
-let currentDate = new Date();
-let currentWeekStart = new Date(currentDate);
-currentWeekStart.setDate(currentDate.getDate() - currentDate.getDay() + (currentDate.getDay() === 0 ? -6 : 1) + 1);
-currentWeekStart.setHours(0, 0, 0, 0);
+var DateTime = luxon.DateTime;
+
+// Global variables to keep track of the current date and week start
+let currentDate = DateTime.local();
+let currentWeekStart = currentDate.startOf('week');
 
 function dayIndex(day) {
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -9,16 +10,14 @@ function dayIndex(day) {
 }
 
 function formatDate(date) {
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString(undefined, options);
+    return date.toLocaleString({ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 }
 
 function showPrompt(day, time, occupiedData) {
     const dayIndexValue = dayIndex(day);
-    const dayDate = new Date(currentWeekStart);
-    dayDate.setDate(currentWeekStart.getDate() + dayIndexValue);
+    const dayDate = currentWeekStart.plus({ days: dayIndexValue });
 
-    const formattedDate = dayDate.toISOString().split('T')[0];
+    const formattedDate = dayDate.toISODate();
     const formattedTime = time;
 
     const occupiedSlot = occupiedData.find(entry =>
@@ -51,7 +50,7 @@ function createButton(id, text, clickHandler) {
 }
 
 function createWeeklyPlanner(occupiedData) {
-    console.log('Current Week Start:', new Date(currentWeekStart).toISOString().split('T')[0]);
+    console.log('Current Week Start:', currentWeekStart.toISODate());
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     const hours = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'];
 
@@ -72,6 +71,7 @@ function createWeeklyPlanner(occupiedData) {
     // Button to go to the week of the specified date
     const goToDateButton = createButton('goToDateButton', 'Go to Date', () => goToDate(dateInput.value, occupiedData));
     navCell.appendChild(goToDateButton);
+    
 
 
 
@@ -91,6 +91,7 @@ function createWeeklyPlanner(occupiedData) {
     const todayButton = createButton('todayButton', 'Today', () => updateWeek(0));
     const nextButton = createButton('nextButton', 'Next Week', () => updateWeek(7));
 
+
     navCell.appendChild(prevButton);
     navCell.appendChild(document.createTextNode(' '));
     navCell.appendChild(todayButton);
@@ -105,11 +106,11 @@ function createWeeklyPlanner(occupiedData) {
 
     for (let i = 0; i < 7; i++) {
         const th = document.createElement('th');
-        const dayDate = new Date(currentWeekStart);
-        dayDate.setDate(currentWeekStart.getDate() + i);
-        th.textContent = days[i] + ' ' + dayDate.toISOString().split('T')[0];
+        const dayDate = currentWeekStart.plus({ days: i });
+        th.textContent = days[i] + ' ' + dayDate.toISODate();
         headerRow.appendChild(th);
     }
+
 
     table.appendChild(headerRow);
 
@@ -122,10 +123,9 @@ function createWeeklyPlanner(occupiedData) {
         for (let i = 0; i < 7; i++) {
             const day = days[i];
             const td = document.createElement('td');
-            const dayDate = new Date(currentWeekStart);
-            dayDate.setDate(currentWeekStart.getDate() + i);
+            const dayDate = currentWeekStart.plus({ days: i });
 
-            const formattedDate = dayDate.toISOString().split('T')[0];
+            const formattedDate = dayDate.toISODate();
             const formattedTime = hour;
 
             td.addEventListener('click', () => showPrompt(day, formattedTime, occupiedData));
@@ -159,31 +159,19 @@ function createWeeklyPlanner(occupiedData) {
     container.appendChild(table);
 }
 function getISOWeekNumber(date) {
-    const currentDate = new Date(date);
-    currentDate.setHours(0, 0, 0, 0);
-    currentDate.setDate(currentDate.getDate() + 4 - (currentDate.getDay() || 7)); // Move to Thursday
-
-    const yearStart = new Date(currentDate.getFullYear(), 0, 1);
-    const weekNumber = Math.ceil((((currentDate - yearStart) / 86400000) + 1) / 7);
-
-    return weekNumber;
+    return date.weekNumber;
 }
 
 function getWeekNumber(date) {
-    return getISOWeekNumber(date);
+    return date.weekNumber;
 }
 
-function goToDate(dateString, occupiedData) {
-    if (dateString) {
-        const selectedDate = new Date(dateString);
-        currentWeekStart = new Date(selectedDate);
-        currentWeekStart.setDate(selectedDate.getDate() - selectedDate.getDay() + (selectedDate.getDay() === 0 ? -6 : 1));
-        currentWeekStart.setHours(0, 0, 0, 0);
-
-        console.log('Updated Week to Date:', new Date(currentWeekStart).toISOString().split('T')[0]);
-        createWeeklyPlanner(occupiedData);
+function goToWeek(weekNumber) {
+    if (weekNumber >= 1 && weekNumber <= 52) {
+        const weekDiff = weekNumber - currentWeekStart.weekNumber;
+        updateWeek(weekDiff * 7);
     } else {
-        alert('Please enter a valid date.');
+        alert('Please enter a valid week number (1-52).');
     }
 }
 
@@ -201,22 +189,30 @@ function goToWeek(weekNumber) {
 }
 
 
+function goToDate(dateString, occupiedData) {
+    if (dateString) {
+        const selectedDate = DateTime.fromISO(dateString);
+        currentWeekStart = selectedDate.startOf('week');
 
-
-
-function updateWeek(offset) {
-    if (offset === 0) {
-        currentDate = new Date();
-        currentWeekStart = new Date(currentDate);
-        currentWeekStart.setDate(currentDate.getDate() - currentDate.getDay() + (currentDate.getDay() === 0 ? -6 : 1) + 1);
-        currentWeekStart.setHours(0, 0, 0, 0);
+        console.log('Updated Week to Date:', currentWeekStart.toISODate());
+        createWeeklyPlanner(occupiedData); // Refresh the planner with the new week
     } else {
-        currentWeekStart = new Date(currentWeekStart);
-        currentWeekStart.setDate(currentWeekStart.getDate() + offset);
+        alert('Please enter a valid date.');
+    }
+}
+
+
+
+function updateWeek(offsetDays) {
+    if (offsetDays === 0) {
+        currentDate = DateTime.local();
+        currentWeekStart = currentDate.startOf('week');
+    } else {
+        currentWeekStart = currentWeekStart.plus({ days: offsetDays });
     }
 
-    console.log('Updated Week:', new Date(currentWeekStart).toISOString().split('T')[0]);
-    console.log('Current Date:', currentDate.toISOString().split('T')[0]);
+    console.log('Updated Week:', currentWeekStart.toISODate());
+    console.log('Current Date:', currentDate.toISO()); // Updated this line
     console.log('Current Week Start:', new Date(currentWeekStart).toISOString().split('T')[0]);
 
     const selectedEmployeeId = document.getElementById('dynamicSelectEmployeePlanner').value;
--- code/agentPage/template.php
+++ code/agentPage/template.php
@@ -3,7 +3,7 @@
     <head>
       <title>Ma page</title>
       <meta charset="utf-8">
-      
+      <script src="luxon.js"></script>
       <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
       <style>
     table {
